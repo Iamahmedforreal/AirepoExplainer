370d688dc35f903594810add1f48782ff0cdb051
Ahmeddaud11 <shahwah3344@gmail.com>
2026-04-29 02:57:19 +0300

    fixed router to be more clean

--- app/router/urlRoute.py
+++ app/router/urlRoute.py
@@ -1,11 +1,18 @@
+import os
+
 from fastapi import APIRouter, Depends, HTTPException, Request
-from app.schema.urlSchema import TrustedGitHubRepoLink
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.celery.task import clone_repository
+from app.config.config import settings
 from app.models.db import get_db
-from app.services.urlService import extract_repo_info, save_repo
+from app.schema.urlSchema import TrustedGitHubRepoLink
+from app.services.urlService import check_existing_repo, extract_repo_info, save_repo
 from app.utils.utils import authenticate_and_get_user_id
 
-router= APIRouter(prefix="/api", tags=["repositories"])
+
+router = APIRouter(prefix="/api", tags=["repositories"])
+
 
 @router.post("/repos")
 async def submitting_url(
@@ -16,14 +23,23 @@ async def submitting_url(
     try:
         user_details = authenticate_and_get_user_id(request)
         user_id = user_details.get("user_id")
-        metadata = await extract_repo_info(str(payload.url))
-        result = await save_repo(user_id, metadata, db)
+
+        metadata, owner, repo_name = await extract_repo_info(str(payload.url))
+
+        existing_repo = await check_existing_repo(user_id, metadata.get("githubUrl"), db)
+        if existing_repo:
+            return {"message": "Repository already indexed"}
+
+        await save_repo(user_id, metadata, db)
+
+        local_path = os.path.join(settings.clone_base_dir, f"{owner}_{repo_name}")
+        clone_repository.delay(metadata.get("githubUrl"), local_path)
+
         return {"message": "Repository indexed successfully"}
+
     except ValueError as error:
         raise HTTPException(status_code=400, detail=str(error))
     except HTTPException:
         raise
     except Exception as error:
         raise HTTPException(status_code=500, detail=str(error))
-
-        
