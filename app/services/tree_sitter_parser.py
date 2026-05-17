19ea9b0eb908a86319d97d8498f0fcb170397488
Ahmeddaud11 <shahwah3344@gmail.com>
2026-05-17 22:11:33 +0300

    made function for parsing files and ingine

--- app/services/tree_sitter_parser.py
+++ app/services/tree_sitter_parser.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from tree_sitter import Language, Parser
 
 EXTENSION_TO_LANGUAGE = {
     ".py":   "python",
@@ -7,21 +8,25 @@ EXTENSION_TO_LANGUAGE = {
 }
 
 
-def extract_languages_from_clean_files(cleaned_files: list[dict]) -> str | set[str]:
-    """Return list of TreeSitter language names for our repo, or None if unsupported ."""
+def detect_language(path: str) -> str | None:
+    """Return TreeSitter language name for this file, or None if unsupported."""
+    if path not in EXTENSION_TO_LANGUAGE:
+        raise ValueError(f"Unsupported file type: {path}")
+    ext = Path(path).suffix.lower()
+    return EXTENSION_TO_LANGUAGE.get(ext)
 
-    detect_language = set()
+"""functions to parse code using each files language and tree-sitter grammars. """
+def parser(language: str):
+    if language == "python":
+        import tree_sitter_python as lang_modula
+    elif language == "javascript":
+        import tree_sitter_javascript as lang_modula
+    elif language == "typescript":
+        import tree_sitter_typescript as lang_modula
 
-    for file in cleaned_files:
-        file_path = file.get("path", " ")
-        ext = Path(file_path).suffix.lower()
-
-        language = EXTENSION_TO_LANGUAGE.get(ext)
-        if language:
-            detect_language.add(language)
-
-    return detect_language
-        
+    lang = Language(lang_modula.language())
+    parser = Parser(lang)
+    return parser
 
     
   
