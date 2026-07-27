fefdceb95b5d6a58f9cc6c7fec5fdb51b57b186b
Ahmeddaud11 <shahwah3344@gmail.com>
2026-07-28 02:35:31 +0300

    fixed problem that could haappen iguess

--- app/config/app_config.py
+++ app/config/app_config.py
@@ -6,13 +6,14 @@ class Settings(BaseSettings):
     github_api_key: str
     clerk_webhook_secret: str
     jwt_publik_key: str
-    clerk_secret_key: str = Field(..., validation_alias="CLEERK_SCERET_KEY")
-    openai_api_key: str | None = None
+    clerk_secret_key: str
+    openai_api_key: str
     embedding_model: str = "text-embedding-3-small"
     embedding_dimensions: int = 1536
     embedding_batch_size: int = 64
     embedding_max_input_chars: int = 12000
-    clone_base_dir: str = r"C:\Users\hp\repoAiProject\cloned_repos"
+    clone_base_dir: str 
+    authorized_parties: list[str] = ["http://localhost:5173"]
 
     class Config:
         env_file = ".env"
